cf7651936c6ce10d043cf3b497235b09c8db7905
Ilmari Saari <Hopeaneito@hopeaneito.local>
2018-03-02 15:53:16 +0200

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Ilmari Saari on 01/03/2018.
+//  Copyright © 2018 Ilmari Saari. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title : String = ""
+    var done : Bool = false
+    
+}
